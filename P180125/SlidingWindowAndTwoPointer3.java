2b087bd322cebbd7c328a019f566bec6e1314177
Kumar Sambhav <sambhav26k@gmail.com>
2025-01-19 13:36:38 +0530

    18th Jan

---
+++ P180125/SlidingWindowAndTwoPointer3.java
@@ -0,0 +1,29 @@
+package P180125;
+
+public class SlidingWindowAndTwoPointer3 {
+    
+    static int binarySubArraysWithSum(int[] nums, int goal) {
+        return atMost(nums, goal) - atMost(nums, goal - 1);
+    }
+    
+    static int atMost(int[] nums, int goal) {
+        if (goal < 0) return 0;
+        int left = 0, sum = 0, count = 0;
+        for (int right = 0; right < nums.length; right++) {
+            sum += nums[right];
+            while (sum > goal) {
+                sum -= nums[left];
+                left++;
+            }
+            count = count + (right - left + 1);
+        }
+        return count;
+    }
+    
+    
+    public static void main(String[] args) {
+        int[] nums = {1, 0, 1, 0, 1};
+        System.out.println(binarySubArraysWithSum(nums, 2));
+    }
+    
+}
